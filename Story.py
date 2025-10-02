43108dedbbd676296f9182dd8e7b258eeee72eea
816231-hins <816231@manadulted.ac.uk>
2025-10-02 13:05:20 +0000

    completed step 2

--- Story.py
+++ Story.py
@@ -1,3 +1,6 @@
 name = input('What is your name?\n')
 
 print(f'Hello {name}!\nLets start this story together!')
+favourite_colour = input("Enter your favourite colour : ")
+favourite_hobby = input("Enter your hobby :")
+print(f"Your favourite color is {favourite_colour} and your hobby is {favourite_hobby} .")
